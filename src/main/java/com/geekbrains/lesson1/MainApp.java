2095a51d4f66b48eeec93d57cd435f6b119c4c61
Sergey <forgit@list.ru>
2021-12-02 11:23:57 +0300

    Lesson1 Begin

---
+++ src/main/java/com/geekbrains/lesson1/MainApp.java
@@ -0,0 +1,10 @@
+package com.geekbrains.lesson1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", "white", 2);
+        Cat cat2 = new Cat("Murzik", "black", 4);
+        cat.info();
+        cat2.info();
+    }
+}
