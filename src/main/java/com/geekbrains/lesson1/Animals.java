35ee8f79bf5df49c57addd283368b2d041b8474d
Forgit1212 <forgit@list.ru>
2021-12-04 07:02:12 +0300

    git commit -m "Add abstract Animals"

---
+++ src/main/java/com/geekbrains/lesson1/Animals.java
@@ -0,0 +1,10 @@
+package com.geekbrains.lesson1;
+
+public abstract class Animals {
+    String name;
+    String color;
+    int age;
+
+    public abstract void swim();
+    public abstract void run();
+}
